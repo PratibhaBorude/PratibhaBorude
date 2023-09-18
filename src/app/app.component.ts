2d3d6436ca97a7fd4d3dc07bdc7236b84f40d62c
AtharvaK-angularminds <atharva.kavale@angularminds.com>
2023-09-18 23:38:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'exam';
+export class AppComponent implements OnInit {
+  countryData: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.http.get('https://restcountries.com/v3.1/all').subscribe({
+      next: (res) => {
+        this.countryData = res;
+      },
+      error: (err) => {
+        console.log('Something went wrong', err);
+      },
+    });
+  }
 }
